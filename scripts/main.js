78f5bb4007113eec06dc4520cf4c8f040f3236a7
lyanne228 <lyanneloh2017@u.northwestern.edu>
2017-05-11 20:55:27 -0500

    11 MAY 2017 2055PM

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,6 @@
 function main() {
-  $(window).scroll(function() {
-    if(Math.ceil($(window).scrollTop() + $(window).height()) == $(document).height()) {
-      $('.upDownArrows').css('background-image', $('.upDownArrows').css('background-image').replace('down', 'up'));
-    } else {
-      $('.upDownArrows').css('background-image', $('.upDownArrows').css('background-image').replace('up', 'down'));
-    }
-  });
+  arrowScrollChange();
+  $(window).scroll(arrowScrollChange);
   $('.upDownArrows').click(function(){
     if (~$(this).css('background-image').indexOf('down')){
       window.location.href = "#design";
@@ -13,6 +8,14 @@ function main() {
       window.location.href = "#";
     }
   });
+}
+
+function arrowScrollChange() {
+  if(Math.ceil($(window).scrollTop() + $(window).height()) == $(document).height()) {
+    $('.upDownArrows').css('background-image', $('.upDownArrows').css('background-image').replace('down', 'up'));
+  } else {
+    $('.upDownArrows').css('background-image', $('.upDownArrows').css('background-image').replace('up', 'down'));
+  }
 }
 
 $(document).ready(main);
