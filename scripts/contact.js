2c99f6ac9e2bc88be4918d3ff581dc6fb248b325
lyanne228 <lyanneloh2017@u.northwestern.edu>
2017-05-29 19:09:44 -0500

    29 May 2017

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,24 +1,18 @@
 function contactMain(){
   $('#emailMe').hover(function(){
     $(this).append('<p><a href="mailto:lyanne228@gmail.com">lyanne228@gmail.com</a></p>');
-    $(this).css('width', '+=200px');
   }, function(){
     $(this).children('p').remove();
-    $(this).css('width', '-=200px');
   });
   $('#resume').hover(function(){
-    $(this).append('<p>LyanneResume.pdf</p>');
-    $(this).css('width', '+=200px');
+    $(this).append('<p><a href="resume.pdf" target="_blank">LyanneResume.pdf</a></p>');
   }, function(){
     $(this).children('p').remove();
-    $(this).css('width', '-=200px');
   });
   $('#github').hover(function(){
     $(this).append('<p><a href="https://github.com/lyanne228">github.com/lyanne228</a></p>');
-    $(this).css('width', '+=200px');
   }, function(){
     $(this).children('p').remove();
-    $(this).css('width', '-=200px');
   });
 }
 
