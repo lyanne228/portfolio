5ea704396cd8eae2f9bf8f1b9e9da7b64b30d6f8
lyanne228 <lyanneloh2017@u.northwestern.edu>
2017-05-31 16:10:41 -0500

    31 May 2017

--- scripts/contact_me.js
+++ scripts/contact_me.js
@@ -9,7 +9,7 @@ $(function(){
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "contact_me.php",
+                url: "scripts/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
